d022468e2b5a6f232bd75d10f76be75cb6f78cc9
ldupas <louisdupas@net-c.com>
2020-04-29 21:17:53 +0200

    solving conflicts HC

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,30 +9,5 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpClient\HttpClient;
 
-class HomeController extends AbstractController
-{
-    /**
-     * @Route("/home/{country}", name="home")
-     */
-    public function index(string $country, WebcamRepository $webcamRepository)
-    {
-        $webcam = $webcamRepository->findWebcamByCountry($country);
-        return $this->render('home/index.html.twig', [
-            'webcam' => $webcam,
-        ]);
-    }
 
- //   /**
- //    * @Route("/home/{country}", name="home")
- //    */
- //   public function index(string $country, ArtworkRepository $artworkRepository)
- //   {
- //       $artworks = $artworkRepository->findArtworksByCountry($country);
-//
-//        return $this->render('home/index.html.twig', [
-//            'artworks' => $artworks,
-//        ]);
-//    }
-
-}
 
